1d92ad9a353c1b6951447f0875d1ce7aeb56fc4d
BThera <brandonmendozaperez@gmail.com>
2018-08-13 20:06:32 -0500

    Conexion con la base mysql, trabajo en proceso

---
+++ APIARMALO/APIARMALO/Controllers/ConectarBD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace APIARMALO.Controllers
+{
+    public class ConectarBD
+    {
+        private ConectarBD()
+        {
+
+        }
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+        private static ConectarBD _instance = null;
+        public static ConectarBD Instance()
+        {
+            if (_instance == null)
+                _instance = new ConectarBD();
+            return _instance;
+        }
+        public bool IsConnect()
+        {
+            if(Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    return false;
+                string connstring = string.Format("",databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+
+            }
+            return true;
+        }
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
