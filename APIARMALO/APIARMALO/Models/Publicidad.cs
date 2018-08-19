91847f94035b9bf2fc3bde0bbd43646e5c17325f
BThera <brandonmendozaperez@gmail.com>
2018-08-19 16:30:40 -0500

    Comienzo del trabajo con los modelos, modificacion en algunos campos bdd

---
+++ APIARMALO/APIARMALO/Models/Publicidad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIARMALO.Models
+{
+    public class Publicidad
+    {
+        public int ID_EVENT { get; set; }
+        public int ID_ADMIN { get; set; }
+        public int Monto { get; set; }
+        public int Duracion { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ APIARMALO/APIARMALO/Models/Usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIARMALO.Models
+{
+    public class Usuarios
+    {
+        public int USR_ID { get; set; }
+        public string Resume { get; set; }
+        public int TipoUSR { get; set; }
+        public string Nombre_USR { get; set; }
+    }
+}
