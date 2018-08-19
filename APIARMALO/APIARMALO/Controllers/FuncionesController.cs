66330eab75be99248f6b81a9baa0f9b56d42af18
BThera <brandonmendozaperez@gmail.com>
2018-08-19 13:37:54 -0500

    Planteo de metodos finalizado

--- APIARMALO/APIARMALO/Controllers/FuncionesController.cs
+++ APIARMALO/APIARMALO/Controllers/FuncionesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MySql.Data.MySqlClient;
 
 namespace APIARMALO.Controllers
 {
@@ -11,6 +12,7 @@ namespace APIARMALO.Controllers
     {
         public void AgregarEvento() //metodo que consume el api para agregar eventos
         {
+
         }
 
         public void RegistrarUsuario() //metodo que consume el api para crear usuarios
@@ -18,10 +20,36 @@ namespace APIARMALO.Controllers
 
         }
 
-        public void Moderar() //metodo que consume el api para moderar eventos
+        public void ModerarEvento() //metodo que consume el api para moderar eventos
         {
 
         }
 
-    }
+        public void RemoverEvento() //metodo que consume el api para quitar eventos
+        {
+
+        }
+        public void MostrarHistoriaCliente() //metodo que consume el api para mostrar los eventos a los que fue el usuario anteriormente
+        {
+
+        }
+
+        public void MostrarHistoriaOrador() //metodo que consume el api para mostrar los eventos que el charlista dio anteriormente
+        {
+
+        }
+
+        public void PromocionarEvento() //metodo que consume el api para promocionar los eventos en armalo!
+        {
+
+        }
+
+        public void TransferirTaquilla() //metodo que consume el api para transferir los cupos
+        {
+
+        }
+
+
+
+            }
 }
