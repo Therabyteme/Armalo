e640cab3c265612fe17f4b75674a8f36058c7574
BThera <brandonmendozaperez@gmail.com>
2018-08-22 13:14:55 -0500

    Modelado API continua

--- APIARMALO/APIARMALO/Controllers/FuncionesController.cs
+++ APIARMALO/APIARMALO/Controllers/FuncionesController.cs
@@ -10,46 +10,80 @@ namespace APIARMALO.Controllers
 {
     public class FuncionesController : ApiController
     {
+        [Route ("/Evento/AgregarEvento"),HttpPost]
         public void AgregarEvento() //metodo que consume el api para agregar eventos
         {
 
         }
-
+        [Route("/Usuario/AgregarUsuario"), HttpPost]
         public void RegistrarUsuario() //metodo que consume el api para crear usuarios
         {
 
         }
-
+        [Route("/Administracion/Moderar"),HttpGet]
         public void ModerarEvento() //metodo que consume el api para moderar eventos
         {
 
         }
-
+        [Route ("/Evento/RemoverEvento"),HttpDelete]
         public void RemoverEvento() //metodo que consume el api para quitar eventos
         {
 
         }
+        [Route ("/Usuario/HistoriaUser"),HttpGet]
         public void MostrarHistoriaCliente() //metodo que consume el api para mostrar los eventos a los que fue el usuario anteriormente
         {
 
         }
-
+        [Route ("/Usuario/HistoriaOrg"),HttpGet]
         public void MostrarHistoriaOrador() //metodo que consume el api para mostrar los eventos que el charlista dio anteriormente
         {
 
         }
-
+        [Route ("/Administracion/Promo"),HttpPost]
         public void PromocionarEvento() //metodo que consume el api para promocionar los eventos en armalo!
         {
 
         }
-
+        [Route ("/Usuario/Transfer"),HttpPut]
         public void TransferirTaquilla() //metodo que consume el api para transferir los cupos
         {
 
         }
+        [Route ("/Usuario/Quit"),HttpDelete]
+        public void RenunciarCupo()
+        {
 
+        }
+        [Route ("/Evento/CrearStaff"),HttpPost]
+        public void CrearStaff()
+        {
 
+        }
+        [Route ("/Evento/VerAgenda"),HttpGet]
+        public void VerAgenda()
+        {
 
+        }
+        [Route ("/Evento/CrearAgenda"),HttpPost]
+        public void CrearAgenda()
+        {
+
+        }
+        [Route ("/Evento/VerStaff"),HttpGet]
+        public void VerStaff()
+        {
+
+        }
+        [Route ("/Usuario/Resume"),HttpGet]
+        public void VerResume()
+        {
+            
+        }
+        [Route ("/Evento/TomarCupo"),HttpPost]
+        public void TomarCupo()
+        {
+
+        }
             }
 }
--- APIARMALO/APIARMALO/Models/General.cs
+++ APIARMALO/APIARMALO/Models/General.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data;
+using Newtonsoft.Json;
 
 namespace APIARMALO.Models
 {
     public class General
     {
+    }
+    public class Login
+    {
+        [JsonProperty(PropertyName = "usuario")]
+        public string usuario { get; set; }
+        [JsonProperty(PropertyName = "contrasena")]
+        public string contrasena { get; set; }
+    }
+    public class CrearUsuario
+    {
+
+        [JsonProperty(PropertyName = "nombre")]
+        public string usuario { get; set; }
+        [JsonProperty(PropertyName = "contrasena")]
+        public string contrasena { get; set; }
     }
 }
