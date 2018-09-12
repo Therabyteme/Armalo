c8f2ee595929b0636f50efd42232e25e2780dbad
BThera <brandonmendozaperez@gmail.com>
2018-09-12 14:29:32 -0500

    trabajo en autenticacion, pendiente de pruebas y perfeccionar

--- Armalo/Startup.cs
+++ Armalo/Startup.cs
@@ -13,6 +13,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Armalo.Entities;
+
 
 namespace Armalo
 {
@@ -29,15 +36,43 @@ namespace Armalo
         public void ConfigureServices(IServiceCollection services)
         {
             DotNetEnv.Env.Load();
-            // other service configurations go here
-            services.AddDbContextPool<ArmaloContext>( // replace "YourDbContext" with the class name of your DbContext
-                options => options.UseMySql(DotNetEnv.Env.GetString("CONECTION_STRING"), // replace with your Connection String
-                    mysqlOptions =>
-                    {
-                        mysqlOptions.ServerVersion(new Version(8, 0, 12), ServerType.MySql); // replace with your Server Version and Type
-                    }
-            ));
+             // other service configurations go here
+             services.AddDbContextPool<ArmaloContext>( // replace "YourDbContext" with the class name of your DbContext
+                 options => options.UseMySql(DotNetEnv.Env.GetString("CONECTION_STRING"), // replace with your Connection String
+                     mysqlOptions =>
+                     {
+                         mysqlOptions.ServerVersion(new Version(8, 0, 12), ServerType.MySql); // replace with your Server Version and Type
+                     }
+             ));
+            // add dbcontext
+            services.AddDbContext<ApplicationDbContext>();
+            //add identity
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            //add authentication
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // remove default claims
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = Configuration["JwtIssuer"],
+                    ValidAudience = Configuration["JwtIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                    ClockSkew = TimeSpan.Zero //remove delay of token when expire
+                };
+            });
+
+            //add mvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
