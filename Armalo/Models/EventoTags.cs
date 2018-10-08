2c884ed90d2f84275eabf9baa0720f1c926f174a
Luis Alvarez <luis.alvarez.gil@gmail.com>
2018-10-07 23:28:24 -0400

    add models

---
+++ Armalo/Models/EventoTags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armalo.Models
+{
+    public class EventoTags
+    {
+        public int IdEvent { get; set; }
+        public int IdTag { get; set; }
+        public Tags tag { get; set; }
+    }
+}
---
+++ Armalo/Models/Tags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armalo.Models
+{
+    public class Tags
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public int id_evento { get; set; }
+        public ICollection<EventoTags> EventoTags { get; set; }
+    }
+}
